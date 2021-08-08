579c3b71a6a7b7875ca47ae13af46be7b729f6f3
Mauritz Erick <mauritz.erick@gmail.com>
2021-08-09 08:37:02 +1000

    imported chatengine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine, Chatengine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="836d163c-6a86-4076-8e7d-08250e93b3f4"
+            userName="mauritzerick"
+            userSecret="12134668"
+        />
+    )
 }
 
 export default App;
